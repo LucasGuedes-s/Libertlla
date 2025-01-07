fbc538b764d65461f8fab64e0c03ad81e8855820
Lucas Guedes <lucasguedes2908@gmail.com>
2025-01-07 20:52:42 -0300

    Suas alterações no arquivo FormularioDenunciaView.vue

--- front-end/src/views/FormularioDenunciaView.vue
+++ front-end/src/views/FormularioDenunciaView.vue
@@ -1,77 +1,88 @@
 <template>
-    <div class="form-container">   
-        <h1>Denúncia Anônima</h1>
-        <form @submit.prevent="realizarDenuncia">
-            <div class="form-group">
-                <label for="">Tipo de Violência:</label>
-                <select name="tipodeviolencia" id="tipodeviolencia"  v-model="tipodeviolencia">
-                    <option value="violencia_domestica">Violência Doméstica</option>
-                    <option value="agressão_fisica_ou_verbal">Agressão Física ou Verbal</option>
-                    <option value="assedio">Assédio</option>
-                    <option value="abuso_sexual">Abuso Sexual</option>
-                    <option value="ameaca_ou_intimidação">Ameaça ou Intimidação</option>
-                    <option value="perseguicao_ou_stalking">Perseguição ou Stalking</option>
-                </select>
-            </div>
+    <div>
+        <header>
+            <img src="../assets/imagemcabeçalho.png" alt="Imagem de Cabeçalho" class="header-image">
+        </header>
+        <div class="form-container">
+            <h1>Denúncia Anônima</h1>
+            <form @submit.prevent="realizarDenuncia">
+                <div class="form-group">
+                    <label for="">Tipo de Violência:</label>
+                    <select name="tipodeviolencia" id="tipodeviolencia" v-model="tipodeviolencia">
+                        <option value="violencia_domestica">Violência Doméstica</option>
+                        <option value="agressão_fisica_ou_verbal">Agressão Física ou Verbal</option>
+                        <option value="assedio">Assédio</option>
+                        <option value="abuso_sexual">Abuso Sexual</option>
+                        <option value="ameaca_ou_intimidação">Ameaça ou Intimidação</option>
+                        <option value="perseguicao_ou_stalking">Perseguição ou Stalking</option>
+                    </select>
+                </div>
 
-            <div class="form-group">
-                <label for="">Relação com o Agressor:</label>
-                <select name="relacao_agressor" id="relacao_agressor" v-model="relacao_agressor">
-                    <option value="parente_familiar">Parente | Familiar</option>
-                    <option value="desconhecido">Desconhecido(a)</option>
-                    <option value="amigo">Amigo(a)</option>
-                    <option value="companheiro">Companheiro(a)</option>
-                    <option value="ex_companheiro">Ex-companheiro(a)</option>
-                    <option value="colega_trabalho">Colega de trabalho</option>
-                </select>
-            </div>
+                <div class="form-group">
+                    <label for="">Relação com o Agressor:</label>
+                    <select name="relacao_agressor" id="relacao_agressor" v-model="relacao_agressor">
+                        <option value="parente_familiar">Parente | Familiar</option>
+                        <option value="desconhecido">Desconhecido(a)</option>
+                        <option value="amigo">Amigo(a)</option>
+                        <option value="companheiro">Companheiro(a)</option>
+                        <option value="ex_companheiro">Ex-companheiro(a)</option>
+                        <option value="colega_trabalho">Colega de trabalho</option>
+                    </select>
+                </div>
 
-            <div class="form-group" id="descricao">
-                <label for="descricao">Descrição:</label>
-                <textarea id="descricao" name="descricao" rows="8" v-model="descricao"></textarea>
-            </div>
+                <div class="form-group" id="descricao">
+                    <label for="descricao">Descrição:</label>
+                    <textarea id="descricao" name="descricao" rows="8" v-model="descricao"></textarea>
+                </div>
 
-            <div class="form-group" id="data_ocorrido">
-                <label for="data_ocorrido">Data do Ocorrido:</label>
-                <input type="date" id="data" name="data" v-model="data_ocorrido">
-            </div>
+                <div class="form-group" id="data_ocorrido">
+                    <label for="data_ocorrido">Data do Ocorrido:</label>
+                    <input type="date" id="data" name="data" v-model="data_ocorrido">
+                </div>
 
 
-            <div class="form-group" id="anexo_provas">
-                <label for="anexo_provas">Anexar Provas:</label>
-                <input type="file" id="anexo_provas" name="anexo_provas" accept="image/*">
-            </div>
+                <div class="form-group" id="anexo_provas">
+                    <label for="anexo_provas">Anexar Provas:</label>
+                    <input type="file" id="anexo_provas" name="anexo_provas" accept="image/*">
+                </div>
 
-            <div class="form-group" id="local_do_ocorrido">
-                <label for="local_do_ocorrido">Local do Ocorrido:</label>
-                <textarea id="local_do_ocorrido" name="local_do_ocorrido" v-model="local_do_ocorrido" rows="1" placeholder="Rua, Bairro, Nº de Residência..."></textarea>
-            </div>
+                <div class="form-group" id="local_do_ocorrido">
+                    <label for="local_do_ocorrido">Local do Ocorrido:</label>
+                    <textarea id="local_do_ocorrido" name="local_do_ocorrido" v-model="local_do_ocorrido" rows="1"
+                        placeholder="Rua, Bairro, Nº de Residência..."></textarea>
+                </div>
 
-            <button type="submit" click="realizarDenuncia" class="btn_realizardenuncia">Realizar Denúncia!</button>
+                <button type="submit" click="realizarDenuncia" class="btn_realizardenuncia">Realizar Denúncia!</button>
 
-        </form>
+            </form>
+        </div>
     </div>
 </template>
 
 <style scoped>
 body {
     background-color: #4D1032 !important;
-    font-family: 'Montserrat', sans-serif;
+    font-family: "Montserrat", sans-serif;
     margin: 0;
     padding: 0;
 }
 
-header{
-    width: 100%; 
+header img {
+    width: 100%;
+    /* Imagem vai ocupar toda a largura disponível */
     height: auto;
+    /* Manter a proporção da imagem */
 }
 
 h1 {
     margin-top: 10px;
+    margin-bottom: 20px;
+    font-family: "Montserrat", sans-serif;
+    font-weight: bold;
 }
 
 .form-container {
-    width: 90%;
+    width: 95%;
     margin: 48px auto;
     padding: 32px;
     border-radius: 10px;
@@ -82,7 +93,8 @@ h1 {
 form {
     display: grid;
     grid-template-columns: 1fr 1fr;
-    gap: 25px; /* Ajuste o espaço entre os campos */
+    gap: 25px;
+    /* Ajuste o espaço entre os campos */
     align-items: start;
 }
 
@@ -92,10 +104,13 @@ form {
 }
 
 .form-group label {
-    margin-bottom: 10px; /* Ajuste a margem se necessário */
+    margin-bottom: 10px;
+    /* Ajuste a margem se necessário */
 }
 
-.form-group input, select, textarea {
+.form-group input,
+select,
+textarea {
     width: 100%;
     padding: 10px;
     color: white;
@@ -103,20 +118,28 @@ form {
     border-radius: 4px;
     background-color: #4D1032;
     font-family: 'Montserrat', sans-serif;
-    box-sizing: border-box; /* Garantir que padding e border não aumentem o tamanho total */
+    box-sizing: border-box;
+    /* Garantir que padding e border não aumentem o tamanho total */
     resize: none;
 }
 
 ::placeholder {
     color: white;
+    font-size: 14px;
 }
 
 select option:hover {
-    background-color: #ffcc00;  /* Cor de fundo da opção ao passar o mouse */
-    color: white;               /* Cor do texto da opção ao passar o mouse */
+    background-color: #ffcc00;
+    /* Cor de fundo da opção ao passar o mouse */
+    color: white;
+    /* Cor do texto da opção ao passar o mouse */
 }
 
-#descricao, #adicionar_imagem, #local_do_ocorrido, #anexo_provas, #data_ocorrido {
+#descricao,
+#adicionar_imagem,
+#local_do_ocorrido,
+#anexo_provas,
+#data_ocorrido {
     grid-column: 1 / -1;
 }
 
@@ -139,7 +162,8 @@ h2 {
 }
 
 .btn_realizardenuncia:hover {
-    background-color: #FF00AE; /* Cor alterada quando o mouse passa por cima (exemplo: verde mais escuro) */
+    background-color: #FF00AE;
+    /* Cor alterada quando o mouse passa por cima (exemplo: verde mais escuro) */
 }
 </style>
 <script>
@@ -162,10 +186,10 @@ export default {
     beforeUnmount() {
         document.body.style.backgroundColor = ""; // Reseta a cor ao sair da página
     },
-    methods:{
+    methods: {
         async realizarDenuncia() {
             await axios.post("http://localhost:3000/cadastrar/ocorrencia", {
-            ocorrencias: {
+                ocorrencias: {
                     tipo_denuncia: this.tipodeviolencia,
                     tipo_violencia: this.tipodeviolencia,
                     agressor: this.relacao_agressor,
@@ -173,16 +197,16 @@ export default {
                     descricao: this.descricao,
                     local: this.local_do_ocorrido,
                     data_ocorrencia: this.data_ocorrido
-            }
+                }
             })
-            .then((response) => {
-                console.log("Ocorrência cadastrada com sucesso:", response.data);
-            })
-            .catch((error) => {
-                console.error("Erro ao cadastrar a ocorrência:", error);
-            });
+                .then((response) => {
+                    console.log("Ocorrência cadastrada com sucesso:", response.data);
+                })
+                .catch((error) => {
+                    console.error("Erro ao cadastrar a ocorrência:", error);
+                });
 
-    },
+        },
     }
 }
 </script>
