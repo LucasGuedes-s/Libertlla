0eb0b299aceaeaeec9694de5603a54d400ef9822
samuel <samuelpc4567@gmail.com>
2025-01-28 22:54:36 -0300

    Adição da função de verificação do token

--- Back-end/controllers/user.controller.js
+++ Back-end/controllers/user.controller.js
@@ -1,16 +1,20 @@
-const { Router } = require('express')
-const LoginUser = require('../services/user.service')
+const { Router } = require('express');
+const LoginUser = require('../services/user.service');
 
 async function LoginUsuario(req, res, next) {
     try {
-        const Login = await LoginUser.LoginUser(req.body)
+        const Login = await LoginUser.LoginUser(req.body);
+
         res.setHeader('Authorization', `Bearer ${Login.token}`);
-        res.status(200)
-        res.end()
+        res.status(200).json({ 
+            message: "Login bem-sucedido", 
+            token: Login.token, 
+            usuario: Login.usuario
+        });
     } catch (error) {
-        console.error('Erro no login do usuário', error)
-        next(error)
+        console.error('Erro no login do usuário', error);
+        next(error);
     }
 }
 
-module.exports = { LoginUsuario }
+module.exports = { LoginUsuario };
--- Back-end/routes/user.router.js
+++ Back-end/routes/user.router.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/user.controller')
+const validarJWT = require('../middlewares/auth.js')
 
-router.post('/login', controller.LoginUsuario)
+router.post('/login', validarJWT, controller.LoginUsuario)
+
 
 module.exports = router;
