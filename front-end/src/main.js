e081ce6378de379eb19d1ca3deb34d0d649a4226
João Paulo <joaopaulosv068@gmail.com>
2025-02-04 09:07:17 -0300

    Instalação e Configuração do Pinia

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+const pinia = createPinia()
+createApp(App).use(router).use(pinia).mount('#app')
+
 
 Vue.config.productionTip = false
 
