0e3daccfc5ddeaf692b782595eb5cd36230aaae9
Lucas Guedes <lucasguedes2908@gmail.com>
2025-02-12 19:49:14 -0300

    Inicio da tela de ocorrencia

--- front-end/src/views/OcorrenciaView.vue
+++ front-end/src/views/OcorrenciaView.vue
@@ -1,7 +1,50 @@
 <template>
-    <SideBar v-if="sidebarVisible" />
+    <div>
+        <SideBar v-if="sidebarVisible" />
+        <div class="container mt-4" style="margin-left: 250px;">
+            <div class="card">
+                <div class="card-header bg-light">
+                    <h5 class="text-primary">Denúncia - (Código)</h5>
+                </div>
+                <div class="card-body">
+                    <form>
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label for="data" class="form-label">Data:</label>
+                                <input type="date" id="data" class="form-control" v-model="form.data" />
+                            </div>
+                            <div class="col-md-6">
+                                <label for="tipoDenuncia" class="form-label">Tipo de denúncia:</label>
+                                <input type="text" id="tipoDenuncia" class="form-control" v-model="form.tipoDenuncia" />
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label for="relacaoDenunciado" class="form-label">Relação com a Pessoa
+                                    Denunciada:</label>
+                                <input type="text" id="relacaoDenunciado" class="form-control"
+                                    v-model="form.relacaoDenunciado" />
+                            </div>
+                            <div class="col-md-6">
+                                <label for="localOcorrido" class="form-label">Local do Ocorrido:</label>
+                                <input type="text" id="localOcorrido" class="form-control"
+                                    v-model="form.localOcorrido" />
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="descricao" class="form-label">Descrição:</label>
+                            <textarea id="descricao" class="form-control" rows="3" v-model="form.descricao"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="provas" class="form-label">Provas:</label>
+                            <textarea id="provas" class="form-control" rows="3" v-model="form.provas"></textarea>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
-
 <script>
 import SideBar from '@/components/SideBar.vue';
 //import axios from 'axios';
@@ -21,6 +64,14 @@ export default {
         return {
             sidebarVisible: true,
             ocorrencia: [],
+            form: {
+                data: "",
+                tipoDenuncia: "",
+                relacaoDenunciado: "",
+                localOcorrido: "",
+                descricao: "",
+                provas: ""
+            }
         };
     },
     mounted() {
@@ -38,5 +89,4 @@ export default {
 
 
 <style scoped>
-
 </style>
