24ed949f8abd46a20c19b2ff5015b74e00138589
João Paulo <joaopaulosv068@gmail.com>
2025-02-17 19:57:25 -0300

    Estilizando Tela de Não Autorizado

--- front-end/src/views/NaoAutorizadoView.vue
+++ front-end/src/views/NaoAutorizadoView.vue
@@ -1,43 +1,101 @@
 <template>
-    <div class="not-authorized">
-      <h1>Acesso Negado</h1>
-      <p>Você não tem permissão para acessar esta página.</p>
-      <button @click="voltar">Voltar</button>
+  <div class="not-authorized">
+    <div class="content">
+      <h1>Acesso Negado!</h1>
+      <p>
+        Você não tem permissão para acessar esta página.
+      </p>
+      <button @click="voltar">Voltar para Login</button>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    methods: {
-      voltar() {
-        this.$router.push('/login'); // Redireciona para a página inicial
-      }
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    voltar() {
+      this.$router.push('/login'); // Redireciona para a página inicial
     }
-  };
-  </script>
-  
-  <style scoped>
-  .not-authorized {
-    text-align: center;
-    padding: 50px;
-    font-family: Arial, sans-serif;
   }
-  
+};
+</script>
+
+<style scoped>
+.not-authorized {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #54123F; 
+  background-blend-mode: multiply;
+  background-image: url("../assets/stoplogin.png"); /* Substitua pela URL da imagem */
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed; /* Faz a imagem de fundo ficar fixa */
+}
+
+.content {
+  background-color: #54123F; /* Fundo semi-transparente */
+  padding: 50px 50px;
+  border-radius: 8px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  text-align: center;
+  max-width: 500px;
+  width: 100%;
+  border: 1px solid #8b2276;
+}
+
+h1 {
+  font-size: 45px;
+  font-weight: bolder;
+  color: white;
+  margin-bottom: 20px;
+}
+
+p {
+  font-size: 18px;
+  color: white;
+  margin-bottom: 30px;
+}
+
+button {
+  background-color: #8b2276;
+  color: white;
+  border: none;
+  padding: 12px 24px;
+  font-size: 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  width: 100%;
+}
+
+button:hover {
+  background-color: #632256;
+  border: 1px solid #8b2276;
+}
+
+button:focus {
+  outline: none;
+}
+@media (max-width: 768px) {
+  .content {
+    padding: 30px 20px;
+    max-width: 90%;
+  }
+
   h1 {
-    color: red;
+    font-size: 30px;
   }
-  
-  button {
-    background-color: #d9534f;
-    color: white;
-    border: none;
-    padding: 10px 20px;
-    cursor: pointer;
+
+  p {
     font-size: 16px;
-    margin-top: 20px;
   }
-  
-  button:hover {
-    background-color: #c9302c;
+
+  button {
+    padding: 10px 20px;
+    font-size: 14px;
   }
-  </style>
+}
+
+</style>
