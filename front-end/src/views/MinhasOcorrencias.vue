b30fb9dd11844ecbec4fe991316aa35147177752
samuel <samuelpc4567@gmail.com>
2025-02-21 11:09:17 -0300

    Correção na responsvidade da tela de minhas ocorrencias

--- front-end/src/views/MinhasOcorrencias.vue
+++ front-end/src/views/MinhasOcorrencias.vue
@@ -1,87 +1,343 @@
 <template>
-  <div>
-    <div class="minhasocorrencias d-flex">
-      <SideBar v-if="sidebarVisible" />
-      <div class="ocorrencia container">
-        <h1 class="titulo mb-4">Minhas Ocorrências:</h1>
+  <div class="dashboard">
+    <div class="d-flex">
+      <SideBar />
+      <div class="container">
+        <div class="titulo-principal">Minhas Ocorrências</div>
+        <div class="div_ocorrencias">
+          <div class="formulario">
+            <div class="info border rounded p-4 row" v-for="ocorrencia in ocorrencias[0].ocorrencias"
+              :key="ocorrencia.id">
+              <div class="denuncia">
+                <label>Data: </label>
+                <p>{{ formatDate(ocorrencia.data_denuncia) }}</p>
+              </div>
 
-        <div class="ocorrencia-form border rounded p-4 row" v-for="ocorrencia in ocorrencias[0].ocorrencias"
-          :key="ocorrencia.id">
-          <div class="col-12 mb-3">
-            <label>Data:</label>
-            <p>{{ ocorrencia.data_denuncia }}</p>
-          </div>
+              <div class="denuncia">
+                <label>Tipo de Denúncia:</label>
+                <p>{{ ocorrencia.tipo_denuncia }}</p>
+              </div>
 
-          <div class="col-12 mb-3">
-            <label>Tipo de Denúncia:</label>
-            <p>{{ ocorrencia.tipo_denuncia }}</p>
-          </div>
+              <div class="denuncia">
+                <label>Tipo de Violencia:</label>
+                <p>{{ ocorrencia.tipo_violencia }}</p>
+              </div>
 
-          <div class="col-12 mb-3">
-            <label>Tipo de Violencia:</label>
-            <p>{{ ocorrencia.tipo_violencia }}</p>
-          </div>
+              <div class="denuncia">
+                <label>Descrição:</label>
+                <p>{{ ocorrencia.descricao }}</p>
+              </div>
+              <div class="buttons">
+                <router-link :to="`/ocorrencia/${ocorrencia.id}`" class="detalhar-btn">Detalhar</router-link>
 
-          <div class="col-12 mb-3">
-            <label>Descrição:</label>
-            <p>{{ ocorrencia.descricao }}</p>
-          </div>
+                <button type="button" class="pdf-btn" @click="gerarPDF(ocorrencia.id)">Gerar PDF</button>
 
-          <div class="col-12 mt-3 d-flex flex-column flex-md-row justify-content-start gap-3">
-            <router-link :to="`/ocorrencia/${ocorrencia.id}`"
-              class="btn btn-custom-primary w-100 w-md-auto">Detalhar</router-link>
-            <button type="button" class="btn btn-custom-secondary w-100 w-md-auto" @click="gerarPDF(ocorrencia.id)">
-              Gerar PDF</button>
-            <button type="button" class="btn btn-custom-secondary w-100 w-md-auto"
-              @click="abrirModal(ocorrencia.id)">Adicionar
-              Progresso</button>
+                <button type="button" class="btn-modal" @click="abrirModal(ocorrencia.id)">Adicionar Progresso</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <!-- Modal -->
+      <div v-if="modalVisible" :key="modalKey" class="modal-overlay">
+        <div class="modal-content">
+          <div class="quadrado">
+            <h1 class="titulo mb-4">Adicionar Progresso</h1>
+          </div>
+          <form @submit.prevent="adicionarProgresso">
+            <label for="descricao">Descrição:</label>
+            <textarea v-model="descricao" id="descricao" rows="4" placeholder="Adicionar descrição"></textarea>
 
-    <div v-if="modalVisible" :key="modalKey" class="modal-overlay">
-      <div class="modal-content">
-        <div class="quadrado">
-          <h1 class="titulo mb-4">Adicionar Progresso</h1>
+            <div class="form-group" id="adicionar_imagem">
+              <label for="imagem">Anexos:</label>
+              <input type="file" id="imagem" name="imagem" accept="image/*" @change="handleFileChange">
+            </div>
+
+            <div class="modal-actions">
+              <button type="submit" class="btn-salvar">Salvar</button>
+              <button type="button" class="btn-cancelar" @click="fecharModal">Cancelar</button>
+            </div>
+
+          </form>
         </div>
-        <form @submit.prevent="adicionarProgresso">
-          <label for="descricao">Descrição:</label>
-          <textarea v-model="descricao" id="descricao" rows="4" placeholder="Adicionar descrição"></textarea>
-
-          <div class="form-group" id="adicionar_imagem">
-            <label for="imagem">Anexos:</label>
-            <input type="file" id="imagem" name="imagem" accept="image/*" @change="handleFileChange">
-          </div>
-        </form>
-        <div class="modal-actions">
-            <button type="submit" class="btn btn-custom-primary">Salvar</button>
-            <button type="button" class="btn btn-custom-secondary" @click="fecharModal">Cancelar</button>
-          </div>
       </div>
     </div>
   </div>
 </template>
 
+
+<style scoped>
+.dashboard {
+  margin-left: 250px;
+  padding: 20px;
+  border-radius: 100px;
+}
+
+.titulo-principal {
+  font-size: 30px;
+  font-family: "Montserrat", sans-serif;
+  font-weight: bold;
+  margin-top: 20px;
+  margin-left: 7px;
+  color: #9B287B;
+}
+
+.div_ocorrencias {
+  display: flex;
+  gap: 20px;
+  flex-wrap: wrap;
+}
+
+.formulario {
+  flex: 1;
+  min-width: 300px;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+.div_ocorrencias h2 {
+  font-size: 20px;
+  margin-bottom: 20px;
+  color: #8b2276;
+  font-weight: bold;
+}
+
+p {
+  color: #7E7E7E;
+  border: 1px solid #d3d3d3b6;
+  border-radius: 4px;
+  padding: 6px;
+  font-family: "Montserrat", sans-serif;
+  padding: 12px;
+}
+
+label {
+  font-family: "Montserrat", sans-serif;
+  color: rgba(152, 152, 152, 255);
+}
+
+.denuncia {
+  margin-bottom: 15px;
+  /* Ajuste conforme necessário */
+}
+
+.denuncia label {
+  display: block;
+  /* Garante que o label fique acima do p */
+  margin-bottom: 5px;
+  /* Adiciona um pequeno espaço abaixo do label */
+}
+
+.denuncia p {
+  margin-top: 5px;
+  /* Adiciona espaço acima do texto */
+  color: #7E7E7E;
+  padding: 5px;
+}
+
+.info {
+  border-color: #d3d3d3b6;
+  border-style: solid;
+  padding: 20px;
+  margin-bottom: 20px;
+}
+
+
+.info_denuncia h3 {
+  margin-bottom: 10px;
+}
+
+
+.info_denuncia p {
+  margin-bottom: 8px;
+  color: #7E7E7E
+}
+
+
+.buttons {
+  grid-column: 1 / -1;
+  display: flex;
+  justify-content: space-between;
+  gap: 10px;
+}
+
+.detalhar-btn,
+.pdf-btn,
+.btn-modal{
+  background-color: rgba(245, 245, 245, 255);
+  color: rgba(152, 152, 152, 255);
+  border: 1px solid rgba(245, 245, 245, 255);
+  border-radius: 5px;
+  font-size: 14px;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 500;
+  flex: 1;
+  /* Faz com que todos os botões tenham a mesma largura */
+  text-align: center;
+}
+
+/* Estilos do Modal */
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+
+.modal-content {
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
+  width: 90vw;
+  /* Ajuste para aumentar a largura (Ex: 50% da tela) */
+  max-width: 600px;
+  /* Define um tamanho máximo */
+  height: auto;
+  /* Ajuste para aumentar a altura (Ex: 60% da altura da tela) */
+  max-height: 90vh;
+  /* Define um tamanho máximo */
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.modal-content form {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  /* Isso garante que o conteúdo do form seja distribuído entre a parte superior e inferior */
+  margin-top: 60px;
+}
+
+.modal-actions {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+}
+
+textarea {
+  width: 100%;
+  padding: 10px;
+  margin-top: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  resize: none;
+  /* Impede que o usuário redimensione a área de texto */
+}
+
+.quadrado {
+  background-color: #54123F;
+  /* Cor de fundo */
+  color: white;
+  /* Cor do texto */
+  width: 100%;
+  /* Ocupa toda a largura da tela */
+  height: 60px;
+  /* Ajusta a altura conforme necessário */
+  top: 0px;
+  left: 0px;
+  display: flex;
+  position: absolute;
+  align-items: center;
+  justify-content: center;
+  /* Centraliza o conteúdo horizontalmente */
+}
+
+.quadrado .titulo {
+  margin: 0;
+  font-size: 24px;
+  color: white;
+  padding: 0;
+  text-align: center;
+  padding-top: 20px;
+  font-family: "Montserrat", sans-serif;
+}
+
+input[type="file"] {
+  font-size: 14px;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  overflow: hidden;
+}
+
+#adicionar_imagem {
+  grid-column: 1 / -1;
+  margin-top: 20px
+}
+
+#imagem {
+  margin-top: 10px;
+}
+
+button {
+  font-size: 14px;
+  padding: 8px;
+  border-radius: 5px;
+}
+
+.modal-actions button {
+  margin-top: -15px;
+  /* Ajuste a distância que deseja mover para cima */
+}
+.btn-salvar, .btn-cancelar {
+  background-color: rgba(245, 245, 245, 255);
+  color: rgba(152, 152, 152, 255);
+  border: 1px solid rgba(245, 245, 245, 255);
+  border-radius: 5px;
+  font-size: 14px;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 500;
+  width: 49%;
+}
+
+.card-body .form-control {
+  font-family: 'Montserrat', sans-serif;
+  color: rgba(152, 152, 152, 255);
+}
+
+@media (max-width: 768px) {
+  .dashboard {
+    margin-left: 0;
+    padding: 10px;
+  }
+
+  .div_ocorrencias {
+    flex-direction: column;
+    gap: 15px;
+  }
+
+}
+</style>
+
 <script>
 import SideBar from '@/components/SideBar.vue';
 import axios from 'axios';
-import { useAuthStore } from '@/store';
-import router from '@/router';
+import { useAuthStore } from '@/store.js'
+import { formatDate } from '@/utils/dataformatar';
 import Swal from 'sweetalert2';
+import router from '@/router';
 
 export default {
-  components: {
-    SideBar,
-  },
   setup() {
     const store = useAuthStore();
     return {
-      store,
+      store
     };
   },
   data() {
     return {
+      formatDate,
       sidebarVisible: true,
       ocorrencias: [],
       modalVisible: false,
@@ -100,7 +356,7 @@ export default {
   mounted() {
     const user = this.store.usuario.usuario;
     const email = user.email;
-    const token = this.store.token
+    const token = this.store.token;
     axios.get(`http://localhost:3000/ocorrencias/${email}`, {
       headers: {
         Authorization: `Bearer ${token}`
@@ -112,12 +368,10 @@ export default {
         processo.ocorrencias.sort((a, b) => {
           const dateA = new Date(a.data_denuncia);
           const dateB = new Date(b.data_denuncia);
-          return dateA - dateB; 
+          return dateA - dateB;
         });
         processo.ocorrencias.reverse();
       });
-
-
     }).catch(error => {
       if (error.status === 403 || error.status === 401) {
         router.push('/nao-autorizado')
@@ -178,6 +432,9 @@ export default {
       this.selectedFileName = '';
     },
     async adicionarProgresso() {
+      console.log("ID da ocorrência:", this.ocorrenciasId);
+      console.log("Descrição:", this.descricao);
+      console.log("Arquivo:", this.file);
       if (!this.descricao.trim()) {
         Swal.fire({
           icon: 'warning',
@@ -217,213 +474,9 @@ export default {
           });
         });
     }
+  },
+  components: {
+    SideBar
   }
 };
 </script>
-
-<style scoped>
-.titulo {
-  font-size: 30px;
-  font-family: "Montserrat", sans-serif;
-  font-weight: bold;
-  margin-top: 20px;
-  margin-left: 7px;
-  color: #9B287B;
-}
-
-p {
-  color: rgba(152, 152, 152, 255);
-  border: 1px solid #d3d3d3b6;
-  /* Adicionando a borda */
-  border-radius: 4px;
-  /* Arredondando as bordas */
-  padding: 6px;
-}
-
-label {
-  font-family: "Montserrat", sans-serif;
-  color: rgba(152, 152, 152, 255);
-}
-
-.ocorrencia {
-  margin-left: 290px;
-  padding: 20px;
-  border-radius: 100px;
-}
-
-.ocorrencia-form {
-  display: flex;
-  flex-direction: column;
-  margin-top: 30px;
-  border-color: #d3d3d3b6;
-  border-style: solid;
-  width: 96%;
-  padding: 20px;
-  margin-left: 5px;
-  margin-bottom: 20px;
-}
-
-.form-group {
-  margin-bottom: 15px;
-}
-
-.form-group input,
-.form-group textarea {
-  width: 100%;
-  padding: 8px;
-  margin-bottom: 10px;
-}
-
-.btn-custom-primary,
-.btn-custom-secondary {
-  background-color: rgba(245, 245, 245, 255);
-  color: rgba(152, 152, 152, 255);
-  border: 1px solid rgba(245, 245, 245, 255);
-  border-radius: 5px;
-  font-size: 14px;
-  font-family: "Montserrat", sans-serif;
-  font-weight: 500;
-  width: 49%;
-}
-
-.btn-custom-primary:hover,
-.btn-custom-secondary:hover {
-  background-color: rgba(245, 245, 245, 255);
-  /* Mudando a cor de fundo */
-  color: rgba(152, 152, 152, 255);
-  /* Mudando a cor do texto */
-  border: 1px solid rgba(245, 245, 245, 255);
-  /* Mudando a cor da borda */
-}
-
-/* Estilos do Modal */
-.modal-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
-}
-
-.modal-content {
-  background: white;
-  padding: 20px;
-  border-radius: 8px;
-  width: 90vw;
-  /* Ajuste para aumentar a largura (Ex: 50% da tela) */
-  max-width: 600px;
-  /* Define um tamanho máximo */
-  height: auto;
-  /* Ajuste para aumentar a altura (Ex: 60% da altura da tela) */
-  max-height: 90vh;
-  /* Define um tamanho máximo */
-  overflow: auto; 
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-}
-
-.modal-content form {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  /* Isso garante que o conteúdo do form seja distribuído entre a parte superior e inferior */
-  margin-top: 60px;
-}
-
-.modal-actions {
-  display: flex;
-  justify-content: space-between;
-  margin-top: 20px;
-}
-
-textarea {
-  width: 100%;
-  padding: 10px;
-  margin-top: 10px;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-  resize: none;
-  /* Impede que o usuário redimensione a área de texto */
-}
-
-.quadrado {
-  background-color: #54123F;
-  /* Cor de fundo */
-  color: white;
-  /* Cor do texto */
-  width: 100%;
-  /* Ocupa toda a largura da tela */
-  height: 60px;
-  /* Ajusta a altura conforme necessário */
-  top: 0px;
-  left: 0px;
-  display: flex;
-  position: absolute;
-  align-items: center;
-  justify-content: center;
-  /* Centraliza o conteúdo horizontalmente */
-}
-
-.quadrado .titulo {
-  margin: 0;
-  font-size: 24px;
-  color: white;
-  padding: 0;
-  text-align: center;
-  padding-top: 20px; 
-  font-family: "Montserrat", sans-serif;
-}
-
-input[type="file"] {
-  font-size: 14px;
-  padding: 8px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  overflow: hidden;
-}
-
-#adicionar_imagem {
-  grid-column: 1 / -1;
-  margin-top: 20px
-}
-
-#imagem {
-  margin-top: 10px;
-}
-
-button {
-  font-size: 14px;
-  padding: 8px;
-  border-radius: 5px;
-}
-
-.modal-actions button {
-  margin-top: -15px;
-  /* Ajuste a distância que deseja mover para cima */
-}
-
-@media (min-width: 2048px) {
-  .ocorrencia {
-    width: calc(100% - 300px);
-    /* Reduzindo a largura para evitar sobreposição */
-    max-width: 1800px;
-    margin-left: 300px;
-    /* Deixa espaço para a Sidebar */
-    padding: 20px;
-  }
-
-  .ocorrencia-form {
-    width: 97%;
-    max-width: 97%;
-  }
-}
-</style>
