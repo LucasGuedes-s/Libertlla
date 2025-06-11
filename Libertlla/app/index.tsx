85463007ceb5186dfc5bcb6fb974af0c9fa24bab
João Paulo <joaopaulosv068@gmail.com>
2025-06-11 19:32:20 -0300

    Alterações + Instalação da aplicação para Android

--- Libertlla/app/index.tsx
+++ Libertlla/app/index.tsx
@@ -1,15 +1,19 @@
 import { Text, View } from "react-native";
+import BLEScanner from "../app/BLEScanner";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, padding: 16 }}>
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: 24,
+        }}
+      >
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </View>
+      <BLEScanner />
     </View>
   );
 }
