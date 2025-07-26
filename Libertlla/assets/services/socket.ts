1418978671b6f2722adcaf95b6b1a968bfea4917
João Pedro Barros <jao.pepedro14@gmail.com>
2025-07-26 17:10:43 -0300

    Atualizando TODOS os links do botão do panico para a render

--- Libertlla/assets/services/socket.ts
+++ Libertlla/assets/services/socket.ts
@@ -1,7 +1,7 @@
 
 import { io } from 'socket.io-client';
 
-const socket = io('http://192.168.100.196:3000', {
+const socket = io('https://libertlla.onrender.com', {
   transports: ["websocket"],
 });
  
